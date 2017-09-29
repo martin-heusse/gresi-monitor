a7d8e698998ab1a92fd40ad0c8fea7a0eb172fb6
Martin Heusse <martin.heusse@imag.fr>
2017-09-29 11:37:16 +0200

    detab

--- monitoring.js
+++ monitoring.js
@@ -5,31 +5,31 @@ let myData=[];
 function retrieveMeters(listLoc,dataLoc) {
     // Get the list of serial numbers and call retrieveData() to eventually plot the data
     $.getJSON(listLoc, function(result){
-	    let i=0;
+        let i=0;
             console.log(result.list);
-	    for (i=0;i<result.list.length;i++){
-	        nbMeters=result.list.length;
-		    setTimeout(retrieveData, i*1000,result.list[i],dataLoc);  // setTimeout programs the calls to retrieveData once/second, in order to comply with rbeesolar policy
-	    }
-	});
+        for (i=0;i<result.list.length;i++){
+            nbMeters=result.list.length;
+            setTimeout(retrieveData, i*1000,result.list[i],dataLoc);  // setTimeout programs the calls to retrieveData once/second, in order to comply with rbeesolar policy
+        }
+    });
 }
 function retrieveData(serialNum,dataLoc) {
     $.getJSON(dataLoc+serialNum, function(result){
         // Process the data that was just fetched
-	    console.log(serialNum);
-	    let ithMeterData=[];
-	    myLabels=[];
-	    result.records.forEach(function(item) {
-		    myLabels.push(item.measureDate); // overriding previous ones, but it's always the same thing
-		    ithMeterData.push(item.measure);
-		});  
+        console.log(serialNum);
+        let ithMeterData=[];
+        myLabels=[];
+        result.records.forEach(function(item) {
+            myLabels.push(item.measureDate); // overriding previous ones, but it's always the same thing
+            ithMeterData.push(item.measure);
+        });
 
-	    myData.push({label: serialNum,
-			//	backgroundColor: 'none',
-			borderColor: `hsl(${(50*nbMeterDone)%360}, 100%,50%)`,
-			data: ithMeterData
-			});
-		nbMetersOK++;
+        myData.push({label: serialNum,
+            //  backgroundColor: 'none',
+            borderColor: `hsl(${(50*nbMeterDone)%360}, 100%,50%)`,
+            data: ithMeterData
+            });
+        nbMetersOK++;
         dataRetrieved("+");
     })
     .fail(function() {
@@ -53,35 +53,35 @@ function dataRetrieved(statusChar){
 function doPlot(){
     let ctx = document.getElementById('myChart').getContext('2d');
     let chart = new Chart(ctx, {
-	    // The type of chart we want to create
-	    type: 'line',
-		    
-	    // The data for our dataset
-	    data: {
-		labels:myLabels, // This is always the same -> take the last one
-		datasets: myData
-	    },
-		    
-	    // Configuration options go here
-	    options: {
-		scales: {
-		    // Add label on Y axes
-		    yAxes: [{
-			    scaleLabel: {
-				display: true,
-				labelString: 'Wh'
-			    }
-			}]
-		}
-	    }
-	});
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+        labels:myLabels, // This is always the same -> take the last one
+        datasets: myData
+        },
+
+        // Configuration options go here
+        options: {
+        scales: {
+            // Add label on Y axes
+            yAxes: [{
+                scaleLabel: {
+                display: true,
+                labelString: 'Wh'
+                }
+            }]
+        }
+        }
+    });
 
 }
 
 // This is the function that triggers everything
 $( document ).ready(function() {
     // The URLs are in 2 hidden elements in the HTML
-	retrieveMeters( document.getElementById("listUrl").value,document.getElementById("dataUrl").value);
+    retrieveMeters( document.getElementById("listUrl").value,document.getElementById("dataUrl").value);
     });
 
 
