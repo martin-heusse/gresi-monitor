9f815e31c9973ea8c9501f314df842619c7f69fd
Martin Heusse <martin.heusse@imag.fr>
2018-03-21 22:48:04 +0100

    Add per year production sum

---
+++ tot.js
@@ -0,0 +1,20 @@
+let nbYears=10;
+
+function computeSum(colNum){
+  let sum=0;
+  $(".prod"+colNum).each(function() {
+    let value = $(this).text();
+    // add only if the value is number
+    if(!isNaN(value) && value.length != 0) {
+          sum += parseFloat(value);
+    }
+  })
+  return sum;
+}
+
+$(document).ready(function(){
+$('.totProd').each(function() {
+    console.log(this.id);
+    this.innerHTML=computeSum(this.id);
+});
+});
