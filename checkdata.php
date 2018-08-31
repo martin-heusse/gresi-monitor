f1f85b2a98ea7c4ae7c97cefc18081a9b6abdf40
Martin Heusse <martin.heusse@imag.fr>
2018-08-31 12:09:20 +0200

    Make checkdata a bit more robust (in particular to 1wh reported at random times)

--- checkdata.php
+++ checkdata.php
@@ -33,31 +33,33 @@ if(count($outdated_meters)){$update_pb_str = implode(', ',$outdated_meters )." n
 
 
 #See if a meter did not return a little bit too many 0s
-#First find the fist and last timestamps with non-null production in our time window
+#First find the fist and last timestamps with non-null production in our time window (In fact > 1, as there are sometime glitches with 1 unit at random times)
 $timeWinStart=$now-$nb_hours_prod*3600;
 
-$qr="select min(envelop.time) as tsi, max(envelop.time) as tsf from (select  (r.ts+m.timeoffset) as time, max(r.prod/m.peak_power) as maxprod from ".tp."readings r, ".tp."meters m  where r.ts>$timeWinStart and r.serial=m.serial group by time) as envelop where envelop.maxprod>0";
+$qr="select min(envelop.time) as tsi, max(envelop.time) as tsf from (select  (r.ts+m.timeoffset) as time, max(r.prod/m.peak_power) as maxprod from ".tp."readings r, ".tp."meters m  where r.ts>$timeWinStart and r.serial=m.serial group by time) as envelop where envelop.maxprod>1";
 $select_messages = $db->prepare($qr);
 $select_messages->setFetchMode(PDO::FETCH_ASSOC);
 $select_messages->execute();
 $daylight=($select_messages->fetchAll())[0];
 $tsi=$daylight["tsi"]; $tsf=$daylight["tsf"];
 
-#Find how many zero production ts we have during that time
-$qr="select serial,count(distinct ts) as nbval  from ".tp."readings where prod=0 and ts>? and ts<? and serial=?";
-$select_messages->setFetchMode(PDO::FETCH_KEY_PAIR);
-$select_messages = $db->prepare($qr);
-$zero_prod_meters=[];
-foreach($meters as $m){
-  # time offset in the _other_ direction !!
-  $select_messages->execute(array($tsi-$m["timeoffset"],$tsf-$m["timeoffset"],$m["serial"]));
-  $nb_zero=($select_messages->fetchAll())[0][1];
-  if($nb_zero>$thresh_null_readings){
-    array_push($zero_prod_meters,$m["name"]." : ".number_format($nb_zero/6,2,",",".")." heures.");
+if($tsi!=NULL && $tsf!=NULL){
+  #Find how many zero production ts we have during that time
+  $qr="select serial,count(distinct ts) as nbval  from ".tp."readings where prod=0 and ts>? and ts<? and serial=?";
+  $select_messages->setFetchMode(PDO::FETCH_KEY_PAIR);
+  $select_messages = $db->prepare($qr);
+  $zero_prod_meters=[];
+  foreach($meters as $m){
+    # time offset in the _other_ direction !!
+    $select_messages->execute(array($tsi-$m["timeoffset"],$tsf-$m["timeoffset"],$m["serial"]));
+    $nb_zero=($select_messages->fetchAll())[0][1];
+    if($nb_zero>$thresh_null_readings){
+      array_push($zero_prod_meters,$m["name"]." : ".number_format($nb_zero/6,2,",",".")." heures.");
+    }
   }
-}
-if(count($zero_prod_meters)){
-  $update_pb_str=$update_pb_str."Alerte production nulle : ".implode(', ',$zero_prod_meters )."\n";
+  if(count($zero_prod_meters)){
+    $update_pb_str=$update_pb_str."Alerte production nulle : ".implode(', ',$zero_prod_meters )."\n";
+  }
 }
 if(strlen($update_pb_str)){
   mail(mailadmin,mailalertsubject,$update_pb_str,prepare_header());
