631e778027650bc87aec06629d7208b6d72b2366
Martin Heusse <martin.heusse@imag.fr>
2020-02-03 10:49:36 +0100

    meilleures productions annuelles

--- yearlyprod.php
+++ yearlyprod.php
@@ -22,6 +22,15 @@ function getMeterProd($db,$m,$from,$to){
   return($total);
 }
 
+function tableHead(){
+  echo "<DIV class='sc'><TABLE CLASS='prod'>";
+  echo "<TR><TD>Compteur</TD><TD>Date</TD>";
+}
+function tableFoot(){
+  echo "</TABLE>";
+  echo "</DIV>";
+}
+
 $db = connect_to_db();
 
 pageHeader(nameAppli()." — Productions annuelles");
@@ -29,13 +38,12 @@ pageHeader(nameAppli()." — Productions annuelles");
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="./tot.js"> </script>
 <?php
-echo "<DIV class='sc'><TABLE CLASS='prod'>";
 
 $meters = get_meter_list_orig($db);
 
 $grandTotal=0;
 
-echo "<TR><TD>Compteur</TD><TD>Date</TD>";
+tableHead();
 
 for ($i=0;$i<$MAX_YEARS;$i++){
   echo "<TD>". ($i + 1) ."</TD>";
@@ -51,8 +59,8 @@ foreach($meters as $m){
   $m0=date("m",$m["fisrtts"]);
   $d0=date("d",$m["fisrtts"]);
   $from=$m["fisrtts"];
-  $y1=$y0+1;
   echo "<TD> $d0/$m0/$y0 </TD>";
+  $y1=$y0+1;
   for ($i=0;$i<$MAX_YEARS;$i++){
     $to=strtotime("$y1-$m0-$d0");
     $curprd=getMeterProd($db,$m,$from,$to);
@@ -68,18 +76,28 @@ for ($i=0;$i<$MAX_YEARS;$i++){
 }
 echo "</B></TR>\n";
 
-echo "</TABLE>";
+tableFoot();
 echo "<BR/><B>Grand Total: ".round($grandTotal)." kWh</B>";
-echo "</DIV>";
 
 // Maintenant, par demi-année
 echo "<h2>Totaux par demi-année</h2>";
-echo "<DIV class='sc'><TABLE CLASS='prod'>";
 
-echo "<TR><TD>Compteur</TD><TD>Date</TD>";
+// Année courante
+$yn=date("Y");
+// Première année ? sera $ys
+$ys=$yn;
+foreach($meters as $m){
+  $y=date("Y",$m["fisrtts"]);
+  if($y<$ys){$ys=$y;};
+}
+$nbYears=$yn-$ys+1;
 
-for ($i=0;$i<$MAX_YEARS*2;$i++){
-  echo "<TD>". round(($i + 1)/2,0) .(($i + 1)%2?"a":"b")."</TD>";
+
+tableHead();
+
+for ($i=0;$i<$nbYears;$i++){
+  echo "<TD>".($ys+$i)."a</TD>";
+  echo "<TD>".($ys+$i)."b</TD>";
 }
 echo "</TR>";
 
@@ -88,28 +106,52 @@ foreach($meters as $m){
 //ex : serial] => 210000217 [name] => name [localization] => [fisrtts] => 1512994200 [lastts] => 1518526200 [peak_power] => 8.1 [timeoffset] => 1200 
   echo "<TR>";
   echo "<TD>".$m["name"]."</TD>";
-  $y0=date("Y",$m["fisrtts"]);
+  $y0=date("y",$m["fisrtts"]);
   $m0=date("m",$m["fisrtts"]);
   $d0=date("d",$m["fisrtts"]);
-  $from=$m["fisrtts"];
-  $y1=$y0+1;
   echo "<TD> $d0/$m0/$y0 </TD>";
-  for ($i=0;$i<$MAX_YEARS;$i++){
+  $y0=$ys;
+  for ($i=0;$i<$nbYears;$i++){
+    $from=strtotime("$y0-01-01");
+    $to=strtotime("$y0-$m0-$d0");
+    $curprd=getMeterProd($db,$m,$from,$to);
+    echo "<TD align='right'> ".round($curprd,1)."    </TD>";
+    $from=$to;
+    $y1=$y0+1;
     $to=strtotime("$y1-01-01");
     $curprd=getMeterProd($db,$m,$from,$to);
     echo "<TD align='right'> ".round($curprd,1)."    </TD>";
-    $from1=$to;
-    $to=strtotime("$y1-$m0-$d0");
-    $curprd=getMeterProd($db,$m,$from1,$to);
-    echo "<TD align='right'> ".round($curprd,1)."    </TD>";
-    $from=$to;$y1=$y1+1;
+    $y0=$y1;
   }
   echo "</TR>\n";
 }
 
-echo "</TABLE>";
-echo "</DIV>";
+tableFoot();
 
+echo "<h2>Totaux par année civile</h2>";
+tableHead();
+for ($i=0;$i<$nbYears;$i++){
+  echo "<TD>".($ys+$i)."</TD>";
+}
+echo "</TR>";
+foreach($meters as $m){
+  echo "<TR>";
+  echo "<TD>".$m["name"]."</TD>";
+  $y0=date("Y",$m["fisrtts"]);
+  $m0=date("m",$m["fisrtts"]);
+  $d0=date("d",$m["fisrtts"]);
+  $from=$m["fisrtts"];
+  echo "<TD> $d0/$m0/$y0 </TD>";
+
+  for ($i=0;$i<$nbYears;$i++){
+    $y1=$ys+$i;
+    $y2=$y1+1;
+    $from=strtotime("$y1-01-01");$to=strtotime("$y2-01-01");
+    $curprd=getMeterProd($db,$m,$from,$to);
+    echo "<TD align='right'> ".round($curprd,1)."    </TD>";
+  }
+  echo "</TR>\n";
+}
 
 pageFoot();
 ?>
