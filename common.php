e6be0cb132cffce68e80fc28c7e364a65cc14362
Kevin Traini <kevin@ktraini.com>
2022-03-03 17:24:21 +0100

    Add function get_readings_rbee

--- common.php
+++ common.php
@@ -94,6 +94,54 @@ function get_meter_list_check($db)
   return $select_messages->fetchAll();
 }
 
+function get_readings_rbee($start, $end, $second)
+{
+    // Create a temporary table with the period timestamps
+    $qr = "CREATE TEMPORARY TABLE all_ts (
+        ts integer unsigned NOT NULL,
+        PRIMARY KEY (ts)
+    );";
+    $prepare_variables = $db->prepare($qr);
+    $prepare_variables->setFetchMode(PDO::FETCH_ASSOC);
+    $prepare_variables->execute();
+    // Fill it
+    $qr = "INSERT INTO all_ts (ts) VALUES (" . implode("), (", range($start, $end, $second)) . ");";
+    $prepare_variables = $db->prepare($qr);
+    $prepare_variables->setFetchMode(PDO::FETCH_ASSOC);
+    $prepare_variables->execute();
+
+    // Create the final query to grab data
+    $qr = "-- Add -1 (null) for all missing values over the period
+    SELECT ts AS ts, -1 AS prod
+        FROM all_ts
+        WHERE all_ts.ts NOT IN ( SELECT ts FROM " . tp . "readings WHERE serial=@serial AND (ts BETWEEN @ts_start AND @ts_end))
+    UNION
+    -- Select prod values for a device over the period
+    SELECT ts+0 as ts, prod
+        FROM " . tp . "readings as tr
+        WHERE serial=@serial AND (tr.ts BETWEEN @ts_start and @ts_end)
+    ORDER BY ts;";
+
+    // Set variables used in the query
+    $prepare_variables = $db->prepare("SET @ts_start = ?;");
+    $prepare_variables->setFetchMode(PDO::FETCH_ASSOC);
+    $prepare_variables->execute(array($start));
+    $prepare_variables = $db->prepare("SET @ts_end = ?;");
+    $prepare_variables->setFetchMode(PDO::FETCH_ASSOC);
+    $prepare_variables->execute(array($end));
+    $prepare_variables = $db->prepare("SET @serial = ?;");
+    $prepare_variables->setFetchMode(PDO::FETCH_ASSOC);
+    $prepare_variables->execute(array($_GET['serial']));
+
+    // Trigger the query
+    $select_messages = $db->prepare($qr);
+    $select_messages->setFetchMode(PDO::FETCH_ASSOC);
+    $select_messages->execute($reqArgs);
+
+    // Send the content
+    return $select_messages->fetchAll();
+}
+
 function get_readings_ticpmepmi($start, $end, $second)
 {
     $db = connect_to_db();
