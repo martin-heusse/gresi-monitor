5bc22d2140b1d204f817743eae335178dcfc7b0e
Oliv4945 <oliv4945@gmail.com>
2022-03-16 23:58:55 +0100

    Use server's protocol

--- monitor-constants.php
+++ monitor-constants.php
@@ -1,4 +1,18 @@
 <?php
+
+// From https://stackoverflow.com/a/14270161
+if (isset($_SERVER['HTTPS']) &&
+        ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $protocol = 'https://';
+}
+else {
+    $protocol = 'http://';
+}
+
+
 # This variable MUST have a trailing slash character
-define('baseUrl','http://localhost/~heusse/Monitor/')
+define('baseUrl',$protocol.'localhost/~heusse/Monitor/')
+
 ?>
